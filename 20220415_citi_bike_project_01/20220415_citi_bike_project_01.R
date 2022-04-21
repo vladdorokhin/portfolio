c1ac7fde31df74dee890cceb6c31d3be4de1db3b
Vlad Dorokhin <103497343+vladdorokhin@users.noreply.github.com>
2022-04-20 23:58:20 -0400

    Update 20220415_citi_bike_project_01.R

--- 20220415_citi_bike_project_01/20220415_citi_bike_project_01.R
+++ 20220415_citi_bike_project_01/20220415_citi_bike_project_01.R
@@ -1,6 +1,11 @@
 # The code below is a part of Vlad Dorokhin's portfolio research project: Citi Bike Bike-Share Analysis (03/2021-03/2022)
 # More information and additional files can be found here: https://github.com/vladdorokhin/portfolio/tree/main/20220415_citi_bike_project_01
 
+# Copyright (C) [2022] [Vlad Dorokhin]
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
+
 # Install and load the project-related packages
 library(tidyverse)
 library(janitor)
@@ -206,3 +211,8 @@ ggmap(nyc_stamen_map, darken = c(0.8, "white")) +
 
 # The code above is a part of Vlad Dorokhin's portfolio research project: Citi Bike Bike-Share Analysis (03/2021-03/2022)
 # More information and additional files can be found here: https://github.com/vladdorokhin/portfolio/tree/main/20220415_citi_bike_project_01
+
+# Copyright (C) [2022] [Vlad Dorokhin]
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
